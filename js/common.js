7ebaf3e2a6bd0396bbd231988ab578e4b1338740
etopbg <asdf982034789@qq.com>
2026-01-19 09:24:34 +0800

    update!!!

--- js/common.js
+++ js/common.js
@@ -30,6 +30,21 @@
         document.head.appendChild(script);
     }
 
+    function loadAds() {
+        const adsScript = document.createElement('script');
+        adsScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8982726393306273";
+        adsScript.async = true;
+        adsScript.crossOrigin = "anonymous";
+        document.head.appendChild(adsScript);
+    }
+
+    if ('requestIdleCallback' in window) {
+        requestIdleCallback(loadAds, { timeout: 3000 }); // 浏览器空闲时加载
+    } else {
+        window.addEventListener('load', loadAds); // fallback
+    }
+
+
     // 👇 关键：推迟到首屏之后
     if ('requestIdleCallback' in window) {
         requestIdleCallback(initGA, { timeout: 3000 });
