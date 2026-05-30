41b4f0d7e34ce8edea1a8cd8d7ed7076d1384305
etopbg <asdf982034789@qq.com>
2026-05-30 20:24:53 +0800

    update!!!

---
+++ assets/Home-DhPPaRwQ.js
@@ -0,0 +1,1 @@
+import{b as o,o as t,c as r,u as s}from"./index-D8UfwYFg.js";const _={__name:"Home",setup(a){const e=s();return o(()=>{e.push("/article")}),(c,n)=>(t(),r("div"))}};export{_ as default};
