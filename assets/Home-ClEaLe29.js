e2a1669af5ca9418e3e4d05510c0f7893aa7b176
etopbg <asdf982034789@qq.com>
2026-06-02 02:31:11 +0800

    update!!!

--- assets/Home-ClEaLe29.js
+++
@@ -1,1 +0,0 @@
-import{b as o,o as t,c as r,u as s}from"./index-Cu5euwhP.js";const _={__name:"Home",setup(a){const e=s();return o(()=>{e.push("/article")}),(c,n)=>(t(),r("div"))}};export{_ as default};
