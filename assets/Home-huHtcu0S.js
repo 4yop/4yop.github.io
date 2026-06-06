308184ce5991a9893bb3962ae8f01da91a3147aa
etopbg <asdf982034789@qq.com>
2026-06-07 00:43:39 +0800

    update!!!

--- assets/Home-huHtcu0S.js
+++
@@ -1,1 +0,0 @@
-import{b as o,o as t,c as r,u as s}from"./index-BQ0H5nq9.js";const _={__name:"Home",setup(a){const e=s();return o(()=>{e.push("/article")}),(c,n)=>(t(),r("div"))}};export{_ as default};
