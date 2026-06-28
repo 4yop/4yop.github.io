8ead0797da73f048beeb1c76f0672aee7a5aa9dd
etopbg <asdf982034789@qq.com>
2026-06-29 01:05:20 +0800

    update!!!

--- assets/Home-Dro4VFWj.js
+++
@@ -1,1 +0,0 @@
-import{e as o,o as t,c as r,u as s}from"./index-Bd5c6bu2.js";const _={__name:"Home",setup(a){const e=s();return o(()=>{e.push("/article")}),(c,n)=>(t(),r("div"))}};export{_ as default};
