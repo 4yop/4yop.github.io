55fd15612807347dae28a967579ca9aeda3b2ade
etopbg <asdf982034789@qq.com>
2026-07-12 13:53:21 +0800

    update!!!

--- assets/index-BM70pJpS.js
+++
@@ -1,1 +0,0 @@
-import{I as o,U as n,v as u,o as r,L as a}from"./index-D6_W4gED.js";import c from"./SunriseSunsetTool-h_U8B_Qw.js";const _=o({__name:"index",setup(p){const t=a(),s=u(()=>{const e=t.path.match(/\/geography\/sunrise-sunset-([a-z]+)\/?$/);return e?e[1]:""});return(e,l)=>(r(),n(c,{"city-slug":s.value},null,8,["city-slug"]))}});export{_ as default};
